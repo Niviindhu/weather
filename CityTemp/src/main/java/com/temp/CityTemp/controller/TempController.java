992bb4d476d322fbfdafca71bcf9e74d21c6cedc
Niviindhu <79825829+Niviindhu@users.noreply.github.com>
2021-04-22 22:12:37 +0530

    Add files via upload

---
+++ CityTemp/src/main/java/com/temp/CityTemp/controller/TempController.java
@@ -0,0 +1,35 @@
+package com.temp.CityTemp.controller;
+
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class TempController {
+	
+	@GetMapping("/temp")
+	public ModelAndView show(@RequestParam("city") String city,ModelMap model) {
+		 try {
+		 RestTemplate restTemplate = new RestTemplate();
+		 
+		 String response = restTemplate.getForObject("http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=56724058e93c04beb8781191db4e5e34", String.class);
+		 model.put("result",response);
+		 }
+		 catch(Exception e) {
+			 String response="City Not Found";
+			 model.put("result",response);
+		 }
+		 return new ModelAndView("login");
+		 
+	}
+		 
+	
+	@GetMapping(value="/login")
+	public ModelAndView view() {
+		return new ModelAndView("login");
+	}
+
+}
